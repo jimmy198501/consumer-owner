97a42431fc959fbd1af12590698dea3c4b5a96ff
jimmy198501 <jenda879@gmail.com>
2026-08-26 10:54:10 +0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// ⬇⬇⬇ 把這裡換成你 Firebase 專案的設定（專案設定 → 一般 → 你的應用程式）⬇⬇⬇
+export const firebaseConfig = {
+  apiKey: "AIzaSyDI5AM-oSsNbyPv2k0-rktQIg93DJc4wao",
+  authDomain: "personal-ip-cb465.firebaseapp.com",
+  projectId: "personal-ip-cb465",
+  storageBucket: "personal-ip-cb465.firebasestorage.app",
+  messagingSenderId: "814596504347",
+  appId: "1:814596504347:web:690a0e029e3f25974031be"
+};
